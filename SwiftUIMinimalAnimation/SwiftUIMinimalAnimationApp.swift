2c69399c4567887743cd4484b1120bbd79e1551f
TDCIAN <tdcian71@gmail.com>
2022-03-12 20:47:03 +0900

    first commit

---
+++ SwiftUIMinimalAnimation/SwiftUIMinimalAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMinimalAnimationApp.swift
+//  SwiftUIMinimalAnimation
+//
+//  Created by JeongminKim on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMinimalAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIMinimalAnimation/View/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  SwiftUIMinimalAnimation
+//
+//  Created by JeongminKim on 2022/03/12.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
