70545bffca03695380c964f6af5070fe1973a8ec
TDCIAN <tdcian71@gmail.com>
2022-03-12 21:15:41 +0900

    config home

--- SwiftUIMinimalAnimation/View/Home.swift
+++ SwiftUIMinimalAnimation/View/Home.swift
@@ -34,10 +34,38 @@ struct Home: View {
             HStack(spacing: 10) {
                 ForEach(currentWeek, id: \.self) { date in
                     Text(extractDate(date: date))
-                        .fontWeight(.bold)
+                        .fontWeight(isSameDay(date1: currentDay, date2: date) ? .bold : .semibold)
                         .frame(maxWidth: .infinity)
+                        .padding(.vertical, isSameDay(date1: currentDay, date2: date) ? 6 : 0)
+                        .padding(.horizontal, isSameDay(date1: currentDay, date2: date) ? 12 : 0)
+                        .frame(width: isSameDay(date1: currentDay, date2: date) ? 140 : nil)
+                        .background {
+                            Capsule()
+                                .fill(.ultraThinMaterial)
+                                .environment(\.colorScheme, .light)
+                                .opacity(isSameDay(date1: currentDay, date2: date) ? 0.8 : 0)
+                        }
+                        .onTapGesture {
+                            withAnimation {
+                                currentDay = date
+                            }
+                        }
                 }
             }
+            .padding(.top, 10)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Steps")
+                    .fontWeight(.semibold)
+                
+                Text("6,243")
+                    .font(.system(size: 45, weight: .bold))
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 15)
+            
+            // MARK: Fitness Ring View
+            
             
         }
         .padding()
@@ -64,8 +92,19 @@ struct Home: View {
     // MARK: Extracting Custom Date Components
     func extractDate(date: Date) -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "dd"
-        return formatter.string(from: date)
+        formatter.dateFormat = (isSameDay(date1: currentDay, date2: date) ? "dd MMM" : "dd")
+        return (isDateToday(date: date) && isSameDay(date1: currentDay, date2: date) ? "Today, " : "") + formatter.string(from: date)
+    }
+    
+    // MARK: Check Date is Today or Same day
+    func isDateToday(date: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInToday(date)
+    }
+    
+    func isSameDay(date1: Date, date2: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDate(date1, inSameDayAs: date2)
     }
 }
 
